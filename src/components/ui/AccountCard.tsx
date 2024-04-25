563222431b5aab36930cabfb8b33ce1291ef111c
vardhan0604 <harsh062004@gmail.com>
2024-04-25 09:36:51 +0530

    removed extra files

--- src/components/ui/AccountCard.tsx
+++ src/components/ui/AccountCard.tsx
@@ -1,44 +1,44 @@
-import { deductAmount, deleteAccount, getAllAccounts, updateAccount } from '@/lib/actions/account.action';
-import { IAccount } from '@/lib/models/account.model';
-import React from 'react'
+// import { deductAmount, deleteAccount, getAllAccounts, updateAccount } from '@/lib/actions/account.action';
+// import { IAccount } from '@/lib/models/account.model';
+// import React from 'react'
 
-type Props = {
-        key:string
-        account: IAccount
-        setAccounts: React.Dispatch<React.SetStateAction<IAccount[]>>;
-}
+// type Props = {
+//         key:string
+//         account: IAccount
+//         setAccounts: React.Dispatch<React.SetStateAction<IAccount[]>>;
+// }
 
-const AccountCard = (props: Props) => {
-    const { account,setAccounts } = props;
+// const AccountCard = (props: Props) => {
+//     const { account,setAccounts } = props;
 
-    const handleClick=async()=>{
-        // const res= await updateAccount(account._id,undefined,12121221)
-        const res= await deductAmount(account._id,100)
-        // const res= await deleteAccount(account._id)
-        // console.log(key)
-        // console.log(res)
+//     const handleClick=async()=>{
+//         // const res= await updateAccount(account._id,undefined,12121221)
+//         const res= await deductAmount(account._id,100)
+//         // const res= await deleteAccount(account._id)
+//         // console.log(key)
+//         // console.log(res)
 
-        const updated=await getAllAccounts();
-        setAccounts(updated)
-        // console.log(account._id)
-    }
+//         const updated=await getAllAccounts();
+//         setAccounts(updated)
+//         // console.log(account._id)
+//     }
 
-    return (
-        <div className='bg-slate-600 rounded-md ' onClick={handleClick}>
-            <p>Name: {account.name}</p>
-            <p>Balance: {account.balance}</p>
-        </div>
-    )
-}
+//     return (
+//         <div className='bg-slate-600 rounded-md ' onClick={handleClick}>
+//             <p>Name: {account.name}</p>
+//             <p>Balance: {account.balance}</p>
+//         </div>
+//     )
+// }
 
-export default AccountCard
+// export default AccountCard
 
-// {
-//     _id: new ObjectId('65db0527af1acf6fc1a49d8c'),
-//     email: 'harsh062004@gmail.com',
-//     name: 'kotak 811',
-//     balance: 638,
-//     createdAt: 2024-02-25T09:15:19.860Z,
-//     updatedAt: 2024-02-25T09:15:19.860Z,
-//     __v: 0
-//   }
+// // {
+// //     _id: new ObjectId('65db0527af1acf6fc1a49d8c'),
+// //     email: 'harsh062004@gmail.com',
+// //     name: 'kotak 811',
+// //     balance: 638,
+// //     createdAt: 2024-02-25T09:15:19.860Z,
+// //     updatedAt: 2024-02-25T09:15:19.860Z,
+// //     __v: 0
+// //   }
--- src/components/ui/AccountCards.tsx
+++ src/components/ui/AccountCards.tsx
@@ -1,21 +1,21 @@
-"use client"
-import { IAccount } from "@/lib/models/account.model";
-import AccountCard from "./AccountCard";
+// "use client"
+// import { IAccount } from "@/lib/models/account.model";
+// import AccountCard from "./AccountCard";
 
-interface AccountCardsProps {
-    accounts: IAccount[];
-    setAccounts: React.Dispatch<React.SetStateAction<IAccount[]>>;
-}
+// interface AccountCardsProps {
+//     accounts: IAccount[];
+//     setAccounts: React.Dispatch<React.SetStateAction<IAccount[]>>;
+// }
 
-const AccountCards = ({ accounts ,setAccounts }: AccountCardsProps) => {
+// const AccountCards = ({ accounts ,setAccounts }: AccountCardsProps) => {
     
-    return (
-        <div className="flex gap-7">
-            {accounts.map((account: IAccount) => (
-                <AccountCard key={account._id.toString()} account={account} setAccounts={setAccounts}/>
-            ))}
-        </div>
-    );
-};
+//     return (
+//         <div className="flex gap-7">
+//             {accounts.map((account: IAccount) => (
+//                 <AccountCard key={account._id.toString()} account={account} setAccounts={setAccounts}/>
+//             ))}
+//         </div>
+//     );
+// };
 
-export default AccountCards;
+// export default AccountCards;
